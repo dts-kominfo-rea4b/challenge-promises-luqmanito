f4cb9cc3cd18b7f645661d416f841dd2974b2eaf
luqmanito <luqmangrahito@gmail.com>
2022-09-18 14:43:51 +0700

    promise selesai

--- promise.js
+++ promise.js
@@ -1,8 +1,35 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
-// TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let promiseOutput = async function (emosi) {
+  let xxi = await promiseTheaterIXX()
+  let vgc = await promiseTheaterVGC()
+
+  if (emosi == 'marah') {
+    let filterMarah = new Promise((resolve, reject) => {
+      let hasil = xxi.concat(vgc)
+      let filterMarah = hasil.filter(filtered => filtered.hasil == 'marah').length
+      resolve(filterMarah)
+    })
+    return filterMarah
+
+  } else if (emosi == `tidak marah`) {
+    let gakMarah = new Promise((resolve, reject) => {
+      let hasil = xxi.concat(vgc)
+      let gakMarah = hasil.filter(filtered => filtered.hasil == 'tidak marah').length
+      resolve(gakMarah)
+    })
+    return gakMarah
+  }
+}
+
+promiseOutput('marah')
+  .then(result => console.log(result))
+promiseOutput('tidak marah')
+  .then(result => console.log(result))
+
 
 module.exports = {
   promiseOutput,
 };
+
+
