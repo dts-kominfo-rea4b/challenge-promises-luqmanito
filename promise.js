a532c28997771ad3882c1689b9323936cc9875fd
luqmanito <109712257+luqmanito@users.noreply.github.com>
2022-09-18 17:37:33 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,3 +1,5 @@
+// FILE DIKERJAKAN DI BRANCH, BUKAN DI MAIN
+
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
